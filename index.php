6e54df6ef67b6ea44a78e652e885e0509a0381d4
hexy <hexagonhexagon163@gmail.com>
2024-05-10 18:38:48 -0400

    rewrote home

--- index.php
+++ index.php
@@ -3,7 +3,14 @@ $title = 'home';
 require 'templates/top.php';
 ?>
 <main>
-    <p>Hi, I'm Allison, and this is my website!</p>
-    <p><a href="resume.php">resume</a></p>
+    <section>
+        <h2>home</h2>
+        <p>Hey there, I'm Allison (she/her)! I mainly go by <b>hexy</b> on the internet, although that name is usually taken so I'm hexyhexagon, hexagonhexagon, and more rarely dualhexagon. I'm a big fan of anything mathematics, computer science, and computer security, and in my spare time I play a bunch of video games and board games.</p>
+        <p>I built this website mainly as a way to have a single place to host everything I'm doing, as I haven't had a place for that until I made this. This place is a bit light on content right now, but that <b>will</b> be changing, I assure you.</p>
+    </section>
+    <section>
+        <h2>recent activity</h2>
+        <p>I <b>just</b> made this site, so there's nothing here yet. But, maybe someday...</p>
+    </section>
 </main>
 <?php require 'templates/bottom.php' ?>
