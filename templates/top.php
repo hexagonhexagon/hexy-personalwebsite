560635650e243b153b7ce0aea0a145043b5a57ac
hexy <hexagonhexagon163@gmail.com>
2024-05-15 00:19:52 -0400

    mobile layout + resizing youtube embed

--- templates/top.php
+++ templates/top.php
@@ -8,17 +8,19 @@
     </head>
     <body>
         <header>
+            <input type="checkbox" id="hamburger-input">
+            <label for="hamburger-input" id="hamburger-menu"></label>
             <h1>hexlab</h1>
         </header>
-    <div class="navcontainer">
-        <nav>
-            <a href="index.php">home</a>
-            <ul>
-                <li><a href="blog.php">blog</a></li>
-                <li><a href="whyhexagon.php">hexagons?</a></li>
-                <li><a href="resume.php">resume</a></li>
-                <li><a href="talks.php">math talks</a></li>
-                <li><a href="mods.php">factorio mods</a></li>
-            </ul>
-        </nav>
-    </div>
+        <div id="nav-container">
+            <nav>
+                <a href="index.php">home</a>
+                <ul>
+                    <li><a href="blog.php">blog</a></li>
+                    <li><a href="whyhexagon.php">hexagons?</a></li>
+                    <li><a href="resume.php">resume</a></li>
+                    <li><a href="talks.php">math talks</a></li>
+                    <li><a href="mods.php">factorio mods</a></li>
+                </ul>
+            </nav>
+        </div>
