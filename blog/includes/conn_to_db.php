6b136148037471030d912582dfe35c6386d4c66b
hexy <hexagonhexagon163@gmail.com>
2024-06-20 22:58:14 -0400

    delete old conn_to_db

--- blog/includes/conn_to_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $db = new PDO('sqlite:' . getenv('BLOG_DB_PATH'));
-    $db->query('SELECT id FROM posts LIMIT 1'); // Must use test query to see if it is actually there
-} catch (PDOException $e) {
-    error_log('there was an error: ' . $e->getMessage());
-    $db = null;
-}
