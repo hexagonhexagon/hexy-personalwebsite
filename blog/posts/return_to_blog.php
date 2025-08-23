f65ae2a1042cd7a70f9b605ac07357d62db6caa3
hexy <hexagonhexagon163@gmail.com>
2025-08-23 15:01:18 -0400

    almost finished first draft of return_to_blog

---
+++ blog/posts/return_to_blog.php
@@ -0,0 +1,44 @@
+<section>
+    <p>Now that I finally have a shiny new blog, it's time to write something! Wait, how long has it been? <b><i>15 MONTHS?!</i></b> What the heck happened?</p>
+
+    <p>What have I been doing? Mostly, not coding this website. If you don't want to listen to me explain my life story for the next 10 minutes, too bad <i>you're already here <b>you hAvE tO rEaD iT nOw</b></i></p>
+</section>
+<section>
+    <h3>the part where i talk about how i haven't updated in 15 months</h3>
+</section>
+<section>
+    <p>My original plan after making my first post was to set up a backend that would be able to interact with the database I've been using to do CRUD (create, remove, update, delete) operations on blog posts, but the scope overwhelmed me and I dropped the project for roughly a year. Since then, I quit my job and honestly had to do a lot of self-improvement in order to get to the point where I can actually do things other than watch YouTube all day.</p>
+    
+    <p>I was definitely in a depressive spiral, and honestly I still am depressed but it can't stop me anymore. I'm pretty sure I'm AuDHD, and it manifests as not being able to start difficult tasks and being constantly starved for stimulation, something YouTube generally feeds. Usually only the structure of school or work could actually break me out of doing nothing, but for the first time in my life I actually managed to create my own structure and fight back the innate desire to do nothing with my life.</p>
+</section>
+<section>
+    <p>I figure I should share what I've been doing in case anyone else felt as stuck as I did, so here's what worked for me (no promises it works for you):</p>
+    <ul>
+        <li>My new therapist has been a massive influence on me. Our sessions mostly involve a lot of body doubling and strategizing around the things I want to do, and it's been very successful. They're responsible for a lot of my positive habits, and I'm very thankful for them.</li>
+        <li><a href="https://obsidian.md/">Obsidian</a>. It's a note-taking app for organizing your thoughts, and it's actually the best thing ever. The way it works appeals to my programmer brain, it's not shoved full of ads and garbage, and everything just works the way I want it to. If it doesn't work, I can usually code it to work the way I want it to. It's been invaluable in tracking my schedule and keeping up with the things I want to do with my life. It's free to use and fully featured for use locally, and you can pay to sync vaults across devices for $4/mo, which I ended up doing since I like the offering so much.</li>
+        <li><a href="https://www.meetup.com/">Meetup</a> is something my therapist suggested I use to try and find people that align with my interests, and it's been hugely successful in my eyes. Most of the coding on this website gets done at a local code meetup around 30 minutes from where I live, and it's nice to be surrounded by other coders. It turns out you really do need to touch grass, who knew.</li>
+        <li>A physical calendar. I hang one up in my room and write stuff I want to do that day on it, and when I do those things I put a little star sticker by it. This is unironically the best thing that has ever happened to me, these stupid star stickers are the best motivator ever. I think that being able to look at my calendar and see all the tasks I've accomplished is part of that, as I do really struggle with acknowledging my victories.</li>
+    </ul>
+</section>
+<section>
+    That's basically it. 
+</section>
+<section>
+    <h3>plans for the future</h3>
+</section>
+<section>
+    <p>Now that I've gotten that out of my system, what's next? Actually, it's moving domains. At the time of writing, my current domain is <b>hex-lab.net</b>, and while I really like that domain, "hexlab" is already a name that <a href="http://www.hexlab.com/">multiple</a> <a href="https://hexlabmakerspace.com/">people</a> <a href="https://www.hexlab.us/">are</a> <a href="https://hex-lab.io/">using</a>. Since I'm not interested in fighting all these people for search results, I'm moving to <b>polytropica.xyz</b>. I'll do a redirect as I have the domain for 9 more months, but I'll eventually relinquish the domain. There will be posts about it.</p>
+    <p>After that? Actual blog posts. About <b>stuff</b>. What stuff?</p>
+</section>
+<section>
+    <h3>STUFF.</h3>
+</section>
+<section>
+    <ul>
+        <li>The two video games that have most occupied my attention are <a href="https://store.steampowered.com/app/2093940/vividstasis/">vivid/stasis</a> and <a href="https://store.steampowered.com/app/603390/Stone_Story_RPG/">Stone Story RPG</a>, the latter of which I got quite good at, and I plan on writing some posts about those.</li>
+        <li>I've actually found and fixed a couple of bugs in games and done some security related stuff, so I might write about it.</li>
+        <li>I also have a bunch of K-12 math floating around in my head from doing a lot of math tutoring, as I have <i>feelings</i> about the way some mathematics is taught in the US.</li> 
+        <li>I attend semi-regular board game meetups and I have some thoughts about some board games that I want to put down to page. Namely, <a>Castles of Mad King Ludwig</a> and <a href="">Quacks of Quedlinburg</a>.</li>
+        <li>I'm working on a cooperative incremental game with two of my friends, but it is not in a state to present to anyone yet, there is a lot of work left to do on it. Do not expect news on this for at least a month.</li>
+    </ul>
+</section>
