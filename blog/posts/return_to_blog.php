59d5ca16306fb42d8911d1992593cedfee0e2d7b
hexy <hexagonhexagon163@gmail.com>
2025-09-08 01:29:08 -0400

    finished text of return blog post

--- blog/posts/return_to_blog.php
+++ blog/posts/return_to_blog.php
@@ -1,44 +1,55 @@
 <section>
-    <p>Now that I finally have a shiny new blog, it's time to write something! Wait, how long has it been? <b><i>15 MONTHS?!</i></b> What the heck happened?</p>
+    <p>Now that I finally have a shiny new blog, it's time to write something! Wait, how long has it been? <b><i>16 MONTHS?!</i></b> What the heck happened?</p>
 
-    <p>What have I been doing? Mostly, not coding this website. If you don't want to listen to me explain my life story for the next 10 minutes, too bad <i>you're already here <b>you hAvE tO rEaD iT nOw</b></i></p>
+    <p>What have I been doing? Basically, I'm just bad at doing things. If you don't want to listen to me explain my life story for the next 10 minutes, too bad <i>you're already here <b>you hAvE tO rEaD iT nOw</b></i></p>
+</section>
+
+<section>
+    <h3>the part where i talk about how i haven't updated in 16 months</h3>
 </section>
 <section>
-    <h3>the part where i talk about how i haven't updated in 15 months</h3>
-</section>
-<section>
-    <p>My original plan after making my first post was to set up a backend that would be able to interact with the database I've been using to do CRUD (create, remove, update, delete) operations on blog posts, but the scope overwhelmed me and I dropped the project for roughly a year. Since then, I quit my job and honestly had to do a lot of self-improvement in order to get to the point where I can actually do things other than watch YouTube all day.</p>
+    <p>My original plan after making my first post was to set up a backend that would be able to interact with the database I've been using to do CRUD (create, remove, update, delete) operations on blog posts, but the scope overwhelmed me and I dropped the project for roughly a year. Since then, I quit my job and honestly had to do a lot of self-improvement to get to the point where I can actually work on things again.</p>
     
-    <p>I was definitely in a depressive spiral, and honestly I still am depressed but it can't stop me anymore. I'm pretty sure I'm AuDHD, and it manifests as not being able to start difficult tasks and being constantly starved for stimulation, something YouTube generally feeds. Usually only the structure of school or work could actually break me out of doing nothing, but for the first time in my life I actually managed to create my own structure and fight back the innate desire to do nothing with my life.</p>
-</section>
-<section>
-    <p>I figure I should share what I've been doing in case anyone else felt as stuck as I did, so here's what worked for me (no promises it works for you):</p>
+    <p>I have been stuck in a depression tar pit for most of my 20's at this point, and honestly I still am depressed but it can't stop me anymore. I'm pretty sure I'm AuDHD, and it manifests as not being able to start difficult tasks and being constantly starved for stimulation. It is fed by what I jokingly call the YouTube demon, which is a black hole for most of my time and energy. It is so much easier to succumb to the YouTube demon than to actually try and make things, which is why it didn't happen for over a year. Usually only the structure of school or work could break me away from it, but for the first time in my life I actually managed to create my own structure and crawl out of the tar pit, which means I get to make things!</p>
+    <p>I figure I should share what I've been doing in case anyone else is struggling like I am, so here's what worked for me (no promises it works for you):</p>
     <ul>
         <li>My new therapist has been a massive influence on me. Our sessions mostly involve a lot of body doubling and strategizing around the things I want to do, and it's been very successful. They're responsible for a lot of my positive habits, and I'm very thankful for them.</li>
-        <li><a href="https://obsidian.md/">Obsidian</a>. It's a note-taking app for organizing your thoughts, and it's actually the best thing ever. The way it works appeals to my programmer brain, it's not shoved full of ads and garbage, and everything just works the way I want it to. If it doesn't work, I can usually code it to work the way I want it to. It's been invaluable in tracking my schedule and keeping up with the things I want to do with my life. It's free to use and fully featured for use locally, and you can pay to sync vaults across devices for $4/mo, which I ended up doing since I like the offering so much.</li>
-        <li><a href="https://www.meetup.com/">Meetup</a> is something my therapist suggested I use to try and find people that align with my interests, and it's been hugely successful in my eyes. Most of the coding on this website gets done at a local code meetup around 30 minutes from where I live, and it's nice to be surrounded by other coders. It turns out you really do need to touch grass, who knew.</li>
+        <li><a href="https://obsidian.md/">Obsidian</a>. It's a note-taking app for organizing your thoughts, and it's actually the best thing ever. It works the way I want it to, it has tons of features, and it's not shoved full of ads and garbage. If it doesn't work, I can usually code it to work the way I want it to. It's been invaluable in tracking my schedule and keeping up with the things I want to do with my life. It's free to use and fully featured for use locally, and you can pay to sync vaults across devices for $4/mo, which I ended up doing since I like the offering so much.</li>
+        <li><a href="https://www.meetup.com/">Meetup</a> is something my therapist suggested I use to try and find people that align with my interests, and it's been hugely successful in my eyes. Most of the coding on this website gets done at a local code meetup around 30 minutes from where I live, and it's nice to be surrounded by other coders. I will say that it is really hard to find people that are like me, even with shared interests, so I have just been brute-forcing social gatherings for the chance that I might find some that I like, and it has been largely successful. I think even going to a bad meetup where you didn't have an amazing time is still so much better for you than staying inside and feeling lonely, and this is largely validated by the past couple days where I didn't go to any meetups and promptly got nothing done.</li>
         <li>A physical calendar. I hang one up in my room and write stuff I want to do that day on it, and when I do those things I put a little star sticker by it. This is unironically the best thing that has ever happened to me, these stupid star stickers are the best motivator ever. I think that being able to look at my calendar and see all the tasks I've accomplished is part of that, as I do really struggle with acknowledging my victories.</li>
     </ul>
+    <p>Another thing I want to say is that it doesn't get better right away. I have been gradually ramping up my productivity over several months, and I still have off-days where I get nothing done. It is very much OK to start small, and I think you kind of have to if you want to have any hope of getting better. To me at least, it is all about acknowledging the small wins you make day over day, and trying to keep that running as long as possible. Even now, after months of work, I am not some sort of reformed non-stop productivity machine, as that is not a real thing you can be. It is all about picking yourself back up again, being kind to your past self, and trying as hard as you can to be better.</p>
+</section>
+
+<section>
+    <h3>the splitting of domains</h3>
 </section>
 <section>
-    That's basically it. 
+    <p>Astute readers may have noticed that I changed domains. This site used to be called "hexlab" at <b>hex-lab.net</b>, and while I really like that domain, "hexlab" is already a name that <a href="http://www.hexlab.com/">multiple</a> <a href="https://hexlabmakerspace.com/">people</a> <a href="https://www.hexlab.us/">are</a> <a href="https://hex-lab.io/">using</a>. Since I'm not interested in fighting all these people for search results, I moved to <b>polytropica.xyz</b>. Not only is it way more unique, it just <i>sounds cooler</i>.</p>
+    <p>Even more astute readers may also have noticed I no longer have my resume on this site. I moved it to a different portfolio site which I will not link here because I am not interested in doxxing myself. I slowly realized that if I wanted to have actual anonymity with what I wanted to say on this site, I kinda needed to separate it from my <i>actual legal name</i>, hence the splitting of the website into personal and professional. It was laziness and a lack of foresight on my part that I put them together in the first place, but they should be pretty darn separate at this point. Here's hoping someone doesn't have it out for me hard enough that they can link the two back together again.</p>
+    <p>I've already set up a redirect from the old site to this new site, so it's just a matter of waiting for search engines to realize I've changed, which I have no control over. I'll keep the old domain for the rest of the year that I bought out and then I'll let the lease expire.</p>
+    <p>Now that the actual website split is done, I am free to do whatever I want, which is probably actual blog posts. About <b>stuff</b>. What stuff?</p>
 </section>
-<section>
-    <h3>plans for the future</h3>
-</section>
-<section>
-    <p>Now that I've gotten that out of my system, what's next? Actually, it's moving domains. At the time of writing, my current domain is <b>hex-lab.net</b>, and while I really like that domain, "hexlab" is already a name that <a href="http://www.hexlab.com/">multiple</a> <a href="https://hexlabmakerspace.com/">people</a> <a href="https://www.hexlab.us/">are</a> <a href="https://hex-lab.io/">using</a>. Since I'm not interested in fighting all these people for search results, I'm moving to <b>polytropica.xyz</b>. I'll do a redirect as I have the domain for 9 more months, but I'll eventually relinquish the domain. There will be posts about it.</p>
-    <p>After that? Actual blog posts. About <b>stuff</b>. What stuff?</p>
-</section>
+
 <section>
     <h3>STUFF.</h3>
 </section>
 <section>
     <ul>
+        <li>I'm probably going to talk about why you should make a blog, and why I decided to bother making one in the first place. I have thoughts I need to put to the page, so that is almost certainly the next post.</li>
         <li>The two video games that have most occupied my attention are <a href="https://store.steampowered.com/app/2093940/vividstasis/">vivid/stasis</a> and <a href="https://store.steampowered.com/app/603390/Stone_Story_RPG/">Stone Story RPG</a>, the latter of which I got quite good at, and I plan on writing some posts about those.</li>
         <li>I've actually found and fixed a couple of bugs in games and done some security related stuff, so I might write about it.</li>
         <li>I also have a bunch of K-12 math floating around in my head from doing a lot of math tutoring, as I have <i>feelings</i> about the way some mathematics is taught in the US.</li> 
-        <li>I attend semi-regular board game meetups and I have some thoughts about some board games that I want to put down to page. Namely, <a>Castles of Mad King Ludwig</a> and <a href="">Quacks of Quedlinburg</a>.</li>
-        <li>I'm working on a cooperative incremental game with two of my friends, but it is not in a state to present to anyone yet, there is a lot of work left to do on it. Do not expect news on this for at least a month.</li>
+        <li>I attend semi-regular board game meetups and I have some thoughts about some board games that I want to put down to page. <a href="https://boardgamegeek.com/boardgame/155426/castles-of-mad-king-ludwig">Castles of Mad King Ludwig</a> and <a href="https://boardgamegeek.com/boardgame/244521/quacks">Quacks of Quedlinburg</a> are the two games I'm thinking about the most right now, but I might think of some others along the way.</li>
+        <li>I'm working on a cooperative incremental game with one of my friends in <a href="https://godotengine.org/">Godot</a>, but it is not in a state to present to anyone yet as there is a lot of work left to do on it. Progress is slow and steady, and my friend has at least a decade of software engineering experience over me, so it has been very educational as well. Do not expect news on this for at least a month. If you're smart enough you can probably find the repo, but I'm not linking it here.</li>
     </ul>
+    <p>How often do I plan on posting? I am going to conservatively aim for once every 2 weeks. It is more than enough time to write something up, and not too much stress that I might fail somehow. If I post faster than that, great! Otherwise, too bad.</p>
+</section>
+
+<section>
+    <h3>it's never too late</h3>
+</section>
+<section>
+    <p>It may have taken me 16 months to get to this point, but I am actually finally here, and that means a lot to me. A lot of things have changed in my life, and I don't think this will just fizzle out like it did 16 months ago. I am very excited to finally be putting the things I want out into the world.</p>
+    <p>If for some reason you've also been putting something off for 16 months, maybe you can start that back up again. I can't say that I know what it is, but it (probably) can't hurt you and it (might) not be too late. I think it's really easy to let old projects rot, but if I could change that then you probably can too.</p>
 </section>
