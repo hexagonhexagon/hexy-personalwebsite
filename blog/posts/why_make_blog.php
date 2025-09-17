5851e5cc434acd170d762b2a7ccf71f444f2e285
hexy <hexagonhexagon163@gmail.com>
2025-09-17 19:13:57 -0400

    partial progress on blog post

---
+++ blog/posts/why_make_blog.php
@@ -0,0 +1,39 @@
+<section>
+    <p>Hahaha, I'm still alive! It hasn't even been two weeks and the third post is here! Anyways, this post is borne out of some recent feelings I've been having about the past few months of my life, so enjoy my unhinged, unedited ramblings (ok I'm not really that unhinged, it's exaggeration for effect you see)</p>
+</section>
+
+<section>
+    <h3>how i made a blog</h3>
+    <p>I initially started this site as a web dev project in 2024, which is a couple of years after I got out of college. I hadn't really had any success applying to jobs, so I wanted to take a shot at doing a personal project. When thinking about what kind of website I wanted to make, my first thought was to make a blog, since that's what all the cool people I followed online had. I had a couple math talks lying around that I wanted to put up for safekeeping, and lofty dreams of making my own games and putting those up here as well, so I set to work.</p>
+
+    <p>As with most things I do, the hardest part is starting. My philosophy was that I wanted to do it from scratch (<a href="https://www.youtube.com/watch?v=Qf56xUKbx24">this video</a> largely sums up why I like this idea) and have full creative control over my website, which led me to VPS (Virtual Private Server) hosting. It gives you full control over the server you rent out and is cheaper, at the cost of being more complicated to set up and requiring more technical know-how. I ended up talking to a coder friend of mine and they recommended the platform DigitalOcean for VPS hosting, so I went ahead and did that.</p>
+
+    <p>One $7.80/month subscription later, and I am the proud owner of a DigitalOcean "droplet", as they call them. I had no idea how to set one up, but that's what search engines are for. Thank you, <a href="https://www.digitalocean.com/community/tutorials/how-to-install-lamp-stack-on-ubuntu">DigitalOcean tutorials</a>. I had some experience doing a <a href="https://en.wikipedia.org/wiki/LAMP_(software_bundle)">LAMP stack</a> for my college web dev class, so that's what I used for the backend.</p>
+
+    <p>The next step was getting a domain name. The same coding friend I talked to about VPS hosting also recommended Namecheap for purchasing a domain name, and I ended up going with "hexlab" for the name of the website, <a href="https://polytropica.xyz/blog/post.php?id=2">a decision I ended up regretting in my other blog post</a>, but this isn't about that. Another $10/year later, I have a domain name! After some manual setup of the <a href="https://easydmarc.com/blog/top-8-most-common-types-of-dns-records/">A and AAAA records</a>, I have a real domain name that points to my server.</p>
+
+    <p>After that, the rest was re-learning all the aspects of web development I had forgotten. There's HTML, CSS, PHP, and SQL, as well as some stuff I didn't know how to do previously, like configuring Apache. It took me around a month of work to get this site from "hello world" to my first blog post about hexagons. There's been a tremendous amount of work behind the scenes after that first month, but the site has not evolved design-wise too much past that first month.</p>
+
+    <p>Then, my progress on the site died in the middle of trying to make the backend work, <a href="https://polytropica.xyz/blog/post.php?id=2">as I conveniently talked about before in that one other blog post</a>. I dragged this project out of the unfinished pile back into the light, and now we are here.</p>
+</section>
+
+<section>
+    <h3>why bother doing all this?</h3>
+    <p>After thinking about it, I realized that the way I've done this is really rather bizarre.</p>
+
+    <ul>
+        <li>If I wanted to make a blog website, I could've just used WordPress and started writing immediately instead of trying to implement all of WordPress's functionality from scratch.</li>
+        <li>If I wanted a resume builder, I didn't <i>have</i> to make a blog website. I could've made any other kind of website, like a portfolio or a video game. Heck, it didn't even have to be a website!</li>
+    </ul>
+
+    <p>Yet I chose to specifically do <i>this</i>, in the hardest way possible. Now that I get out a bit more often, I go to a lot of meetups and tell people about my website. Everyone then asks what framework I used, and they are then bewildered when I tell them "It's just PHP". It's kind of funny, actually.</p>
+
+    <p>I think the answer comes from my main design inspiration, which was <a href="https://msx.horse">em essex/halley labs's personal website</a>. They make great music, but that's beside the point. Not only does it have this great coding/terminal aesthetic, it just feels very raw and real in a way I want to replicate. It's even coded by scratch in PHP, with Notepad++ of all things (note: I did not code this website in Notepad++, I use an IDE like a normal person). But seeing this made me realize the kind of thing I wanted to create: an independent, from-scratch "everything" place where I have full control over theming, content, and presentation.</p>
+
+    <p>I think it has to be this way. At least for me. It's a lot of work to set up something like this from scratch, but I don't want just another WordPress or Substack or whatever people are using these days. I want something that's <i>uniquely mine</i> that gets to show off what I want in my website, and I don't really care if that means it took me 16 months to make a second post. Damn the difficulties, it'll be mine and it'll be mine <i>with style</i>.</p>
+</section>
+
+<section>
+    <h3>blog as a vehicle of self-expression</h3>
+    <p>Maybe this is kind of obvious, but what makes blogging special to me at least is being able to say what you want, how you want it. You own the blog website and you get to write what's in it, so for me it's the ultimate vehicle of self-expression. It's also why I like platforms like YouTube in theory, but in practice YouTube DMCA takedowns and ad monetization make it not very "free" in practice.</p>
+</section>
