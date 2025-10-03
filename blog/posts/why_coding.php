91a0e1a080f240e92b3b7edc1549cef89d766389
hexy <hexagonhexagon163@gmail.com>
2025-10-02 20:26:01 -0400

    start of new post

---
+++ blog/posts/why_coding.php
@@ -0,0 +1,7 @@
+<section>
+    <p>It appears that my blog only exists to tell you why you should do things, as we continue this series of "let's get unreasonably personal about things I care about". That's fine, though. I promise I can write about other topics, I just don't feel like it. Anyways, let's begin.</p>
+</section>
+<section>
+    <h3>test</h3>
+    <p></p>
+</section>
