69101975a9ea678348d1dbf1bb8fc66fbd957056
hexy <hexagonhexagon163@gmail.com>
2026-08-08 14:26:31 -0400

    start of update post

---
+++ blog/posts/update_2026.php
@@ -0,0 +1,6 @@
+<section>
+    <h3>regularly blogging is hard, actually</h3>
+    <p><i>Oh god why has it been 9 months since my last post</i></p>
+    <p>Whatever, it doesn't matter. It turns out a lot happens in 9 months.</p>
+
+</section>
