d29eae29cbe2dc0500bb6bbd37d9094c08172720
hexy <hexagonhexagon163@gmail.com>
2026-08-08 16:18:27 -0400

    continued progress on update post

--- blog/posts/update_2026.php
+++ blog/posts/update_2026.php
@@ -1,6 +1,13 @@
 <section>
-    <h3>regularly blogging is hard, actually</h3>
     <p><i>Oh god why has it been 9 months since my last post</i></p>
-    <p>Whatever, it doesn't matter. It turns out a lot happens in 9 months.</p>
-
+    <p>Whatever, it doesn't matter. It turns out a lot happens in 9 months. Honestly, I can barely remember everything that's happened.</p>
+    <ul>
+        <li>I actually managed to break my arm last winter by slipping on the snow. I broke my humerus just before my elbow, and they had to do some pretty serious surgery and I had to go through 3 months of physical therapy. I don't quite have full motion back but it's close enough that it doesn't interfere with normal everyday tasks.</li>
+        <li>I actually have had a girlfriend for around 8 months now. We met at a makerspace we both went to at the time (that we are now not regularly going to anymore) and hit it off from there. We've now been living together for around a month, and it has been really great. She had a decent size home lab and we are working on it together. We have a minecraft server on it, and I'm getting back into playing minecraft again because of her. I am also blogging again because we both decided to start contributing time to doing that. Her blog is at <a href="https://blog.daemo.nz">blog.daemo.nz</a>. As of writing, there's only one post on it, so we'll see how it goes.</li>
+        <li>The game dev project I was working on collaboratively stalled out in March after I lost motivation to continue working on it. It remains in limbo, and I'm not really sure what I want to do with it.</li>
+    </ul>
+</section>
+<section>
+    <h3>regularly blogging is hard, actually</h3>
+    <p>asfd</p>
 </section>
